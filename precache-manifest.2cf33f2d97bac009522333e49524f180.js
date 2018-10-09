59ab59f615a4b37e5db30faa2e8881e63ed73839
Amanda R Fernandes <amandadcunha@gmail.com>
2018-10-09 14:13:31 -0400

    Updates

---
+++ precache-manifest.2cf33f2d97bac009522333e49524f180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9705b1eaf5e36f9273442ad4eb6e395b",
+    "url": "/react-random-quote/static/media/quotes.9705b1ea.svg"
+  },
+  {
+    "revision": "25824d9f62b11cf28967",
+    "url": "/react-random-quote/static/js/runtime~main.25824d9f.js"
+  },
+  {
+    "revision": "0b58921ebc996e7e9838",
+    "url": "/react-random-quote/static/js/main.0b58921e.chunk.js"
+  },
+  {
+    "revision": "db33ca390c69d604bfe6",
+    "url": "/react-random-quote/static/js/1.db33ca39.chunk.js"
+  },
+  {
+    "revision": "0b58921ebc996e7e9838",
+    "url": "/react-random-quote/static/css/main.fa35a298.chunk.css"
+  },
+  {
+    "revision": "e9e9c05ec202240bf3ed7bb83b2a0fd9",
+    "url": "/react-random-quote/index.html"
+  }
+];
